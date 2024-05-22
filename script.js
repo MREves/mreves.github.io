ddd3edab20ba66e9755230b1f8cab91fa731044c
MREves <43420331+MREves@users.noreply.github.com>
2024-05-22 20:16:26 +0000

    update commit

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ const numberOfCards = data.files.length;
 
 console.log("numberOfCards", numberOfCards);
 
-for (let file of files) {
+for (let file of data.files) {
   console.log(file);
 }
 
-document.onload(event => {
+onload = (event) => {
   const table = document.getElementById("table");
-  const image1 = document.createElement("img", { src: `${path}${data.files[0]}` })
+  const image1 = document.createElement("img")
+  image1.src = path + data.files[0];
 
   table.appendChild(image1);
-});
+}
