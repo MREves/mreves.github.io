27d2493ac4c1960146ded5db9923296549e63191
MREves <43420331+MREves@users.noreply.github.com>
2024-05-22 19:38:34 +0000

    Co-authored-by: Drew Morgan <dangerworm@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,16 @@
+const data = require("data.json");
+
+const numberOfCards = data.files.length;
+
+console.log("numberOfCards", numberOfCards);
+
+for (let file of files) {
+  console.log(file);
+}
+
+document.onload(event => {
+  const table = document.getElementById("table");
+  const image1 = document.createElement("img", { src: data.files[0] })
+
+  table.appendChild(image1);
+});
